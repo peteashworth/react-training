fc81f38e6bc61ee1d04d9e74fa6907e7a09abcdd
Eric W. Greene <eric@ericwgreene.com>
2017-06-26 15:27:27 -0700

    added base url

--- server/widgets-graphql/types/query-type.js
+++ server/widgets-graphql/types/query-type.js
@@ -3,26 +3,11 @@ import fetch from 'node-fetch';
 
 import { widgetType } from './widget-type';
 
-// exercise 3
+// exercise 5
 
-// move your car array to db.json
-// [
-//  "cars": stringified content
-// ]
-
-// configure the cars and car endpoints to pull data from the JSON server
-
-
-const widgetData = [
-  {
-    id: '1', name: 'Widget 1', description: 'Widget 1 Desc',
-    color: 'blue', size: 'large', quantity: 3,
-  },
-  {
-    id: '2', name: 'Widget 2', description: 'Widget 2 Desc',
-    color: 'blue', size: 'large', quantity: 3,
-  }
-];
+// use the context of baseUrl to setup URL for widget, cars, and car
+// use a template literal to build the URL
+// and be sure to encode data values in the URL
 
 export const query = new GraphQLObjectType({
 
@@ -31,20 +16,11 @@ export const query = new GraphQLObjectType({
 
   fields: () => ({
     widgets: {
-
-      // return data type
-      // is always populated by the return of resolve
       type: new GraphQLList(widgetType),
-
       description: 'A list of widgets',
-      resolve: () => {
-
-        return fetch('http://localhost:3000/api/widgets').then(res => res.json());
-
-        // const res = await fetch('http://localhost:3000/api/widgets');
-        // const widgetList = await res.json();
-        // return widgetList;
-      },
+      resolve: (_1, _2, { baseUrl }) =>
+        fetch(`${baseUrl}/widgets`)
+          .then(res => res.json()),
     },
     widget: {
       type: widgetType,
@@ -55,16 +31,9 @@ export const query = new GraphQLObjectType({
           description: 'The id of the widget to load',
         }
       },
-      // first param is the object populating the type
-      // second params are the args
-      // third param is the context object
-      // fourth param is the field info
-      resolve: (_, { widgetId }) => {
-
-        return fetch('http://localhost:3000/api/widgets/' + widgetId)
-          .then(res => res.json());
-
-      },
+      resolve: (_, { widgetId }) =>
+        fetch('http://localhost:3000/api/widgets/' + encodeURIComponent(widgetId))
+          .then(res => res.json()),
     }
   }),
 });
