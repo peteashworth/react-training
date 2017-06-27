88abf321c08bfb7a3e0c0b1f7d9551187f5346b2
Eric W. Greene <eric@ericwgreene.com>
2017-06-27 16:01:54 -0700

    added tool header

--- client/js/app.js
+++ client/js/app.js
@@ -5,6 +5,11 @@ import { ColorTool, ColorToolB } from './components/color-tool.components';
 
 const colors = ['blue', 'green', 'red', 'orange'];
 
+// exercise
+
+// use ToolHeader for Car Tool
+// Move CarTable to its own component
+
 ReactDOM.render(<div>
   <ColorTool colorList={colors} />
 </div>, document.querySelector('main'));
--- client/js/components/color-tool.components.js
+++ client/js/components/color-tool.components.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { ToolHeader } from './tool-header.component';
+
 export class ColorTool extends React.Component {
 
   constructor(props) {
@@ -30,9 +32,7 @@ export class ColorTool extends React.Component {
   render() {
 
     return <div>
-      <header>
-        <h1>Color Tool</h1>
-      </header>
+      <ToolHeader headerText="Color Tool" />
       <ul>
         {this.state.colorList.map((color, i) => <li key={i}>{color}</li>)}
       </ul>
