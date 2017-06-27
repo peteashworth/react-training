b40f815455eed436667f5c48948233af34009561
Eric W. Greene <eric@ericwgreene.com>
2017-06-27 14:39:04 -0700

    added new color form

--- client/js/components/color-tool.components.js
+++ client/js/components/color-tool.components.js
@@ -2,15 +2,38 @@ import React from 'react';
 
 export class ColorTool extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newColor: '',
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
   render() {
 
     return <div>
       <header>
         <h1>Color Tool</h1>
-        <ul>
-          {this.props.colorList.map((color, i) => <li key={i}>{color}</li>)}
-        </ul>
       </header>
+      <ul>
+        {this.props.colorList.map((color, i) => <li key={i}>{color}</li>)}
+      </ul>
+      <form>
+        <div>
+          <label htmlFor="new-color-input">New Color:</label>
+          <input type="text" id="new-color-input" name="newColor"
+            value={this.state.newColor} onChange={this.onChange} />
+        </div>
+      </form>
     </div>;
   }
 
