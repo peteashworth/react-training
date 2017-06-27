58ccf08b779bb8febb0caa12b6c7de762440d50a
Eric W. Greene <eric@ericwgreene.com>
2017-06-27 11:21:39 -0700

    added list of colors

--- client/js/app.js
+++ client/js/app.js
@@ -2,15 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { ColorTool } from './components/color-tool.components';
-// add an import to bring CarTool
 
-// exercise 1
-// create a car tool component which display a header with 'Car Tool'
-
-
-// ReactDOM.render(<ColorTool />, document.querySelector('main'));
-ReactDOM.render(<CarTool />, document.querySelector('main'));
-
-// ReactDOM.render(
-//   React.createElement(ColorTool),
-//   document.querySelector('main'));
+ReactDOM.render(<ColorTool />, document.querySelector('main'));
--- client/js/components/color-tool.components.js
+++ client/js/components/color-tool.components.js
@@ -4,18 +4,22 @@ export class ColorTool extends React.Component {
 
   render() {
 
-    // return React.createElement('div', null,
-    //   React.createElement('header', null,
-    //     React.createElement('h1', null, 'Color Tool')));
+    const colors = ['blue', 'green', 'red', 'hot pink'];
+
+    // exercise 2
+    // display a table of cars
+
+    // please use the table tag, and please display all of the columns
+    // copy your car data from db.json
 
     return <div>
       <header>
         <h1>Color Tool</h1>
+        <ul>
+          {colors.map((color, i) => <li key={i}>{color}</li>)}
+        </ul>
       </header>
     </div>;
-
-    // return React.createElement('div', null, 'some content')React.createElement('div', null 'other content');
-    // return <div>some content</div><div>other content</div>;
   }
 
 }
