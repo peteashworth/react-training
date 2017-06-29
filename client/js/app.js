ceb4531bb0aa8bb16fcf569127c3b19599db6d28
Eric W. Greene <eric@ericwgreene.com>
2017-06-29 15:13:50 -0700

    added connect demo

--- client/js/app.js
+++ client/js/app.js
@@ -1,7 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import { createStore, bindActionCreators } from 'redux';
+import { connect, Provider } from 'react-redux';
 
 const reducer = (state = { result: 0 }, action) => {
-  console.log('state:', state, 'action:', action);
   switch (action.type) {
     case 'ADD':
       return Object.assign({}, state, { result: state.result + action.value });
@@ -14,25 +17,68 @@ const reducer = (state = { result: 0 }, action) => {
 
 const store = createStore(reducer);
 
-
 const addActionCreator = value => ({ type: 'ADD', value });
 const subtractActionCreator = value => ({ type: 'SUBTRACT', value });
 
-const { add, subtract } = bindActionCreators({
+// map the store's application state to the presentational component's props
+const mapStateToProps = (appState) => ({ result: appState.result });
+
+// map the store's bound action creators to the presentational component's props
+const mapDispatchToProps = (dispatch) => bindActionCreators({
   add: addActionCreator,
   subtract: subtractActionCreator,
-}, store.dispatch);
-
-store.subscribe(() => {
-
-  const s = store.getState();
-
-  ReactDOM.render(<MyCalculator result={s.result} add={add} subtract={subtract} />, document.querySelector('main'));
-
-});
-
-// exercise part 1 - build the MyCalcutor component
+}, dispatch);
 
 
 
-// exercise part 2 - implement multiply and divide
+class MyCalculator extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      num: '',
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  onAddClick = () => {
+    this.props.add(Number(this.state.num));
+    this.setState({
+      num: '',
+    });
+  }
+
+  onSubtractClick = () => {
+    this.props.subtract(Number(this.state.num));
+    this.setState({
+      num: '',
+    });
+  }
+
+  render() {
+    return <div>
+      <input type="text" id="num-input" name="num" value={this.state.num} onChange={this.onChange} />
+      <button type="button" onClick={this.onAddClick}>+</button>
+      <button type="button" onClick={this.onSubtractClick}>-</button>
+      <p>Result: {this.props.result}</p>
+    </div>;
+  }
+}
+
+// defines the connection between the store and presentational component
+const containerCreator = connect(mapStateToProps, mapDispatchToProps);
+
+// creates the container which implements the connection between store and the presentational component
+const MyCalculatorContainer = containerCreator(MyCalculator);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <MyCalculatorContainer />
+  </Provider>
+  , document.querySelector('main'));
--- client/js/app_original.js
+++ client/js/app_original.js
@@ -5,6 +5,11 @@ import { ColorTool, } from './components/color-tool.components';
 
 const colors = ['blue', 'green', 'red', 'orange'];
 
+// exercise 
+
+// setup CarTool/ColorTool to use Redux
+// Connect the CarTool/ColorTool component to the Redux store via connect
+
 document.addEventListener('DOMContentLoaded', () => {
 
   ReactDOM.render(<ColorTool colorList={colors} />, document.querySelector('main'));
